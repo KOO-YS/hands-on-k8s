4ca2c76d66a8dd7682783a9929b5d201df72f00c
Yaans <kbeeysk@naver.com>
2024-09-11 21:26:43 +0900

    add deleting containerd

--- setup/01-k8s/k8s-delete.sh
+++ setup/01-k8s/k8s-delete.sh
@@ -5,7 +5,13 @@ systemctl stop kubelet
 systemctl stop containerd
 
 sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
+sudo apt-get remove containerd -y
+
+
+# 불필요한 패키지, 의존성 정리
 sudo apt-get -y autoremove
+sudo apt-get autoclean
+
 rm -rf /etc/kubernetes
 rm -rf /var/lib/etcd
 sudo rm -rf ~/.kube
@@ -37,10 +43,14 @@ sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
 sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
 
 #containerd 파일 삭제
-rm -rf /var/lib/containerd
-rm -rf /var/run/containerd
-rm -rf /opt/containerd
+sudo apt-get purge containerd -y
+sudo rm -rf /var/lib/containerd
+sudo rm -rf /var/run/containerd
+sudo rm -rf /opt/containerd
 
+sudo rm -rf /etc/containerd
+sudo rm -rf /var/lib/containerd
+sudo rm -rf /run/containerd
 
 
 #모든 이미지, 컨테이너 및 볼륨을 삭제
