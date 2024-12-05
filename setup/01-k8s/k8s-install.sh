bb66a00bb9c353cc61870ba8a2c74e85f060fc29
Yaans <kbeeysk@naver.com>
2024-12-05 14:38:58 +0900

    Update k8s-install.sh

--- setup/01-k8s/k8s-install.sh
+++ setup/01-k8s/k8s-install.sh
@@ -67,11 +67,15 @@ sudo apt install -y containerd apt-transport-https
 
 sudo mkdir -p -m 755 /etc/apt/keyrings # Ubuntu 22.04 이전 버전에서 기본으로 제공하지 않는 디렉토리
 # kubernetes 패키지 저장소 공개 서명키 다운로드 (1.27 버전을 특정)
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+#curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# 1.27 release.key expired 이슈로 1.31버전 키 다운로드 ref: https://github.com/kubernetes/release/issues/3818
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
 # kubernetes 1.27 apt 저장소 추가  (마이너 버전 주의)
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
-sudo apt install -y kubeadm kubelet kubectl
+sudo apt update kubeadm=1.27.16-1.1 kubelet=1.27.16-1.1 kubectl=1.27.16-1.1
 # 패키지 자동 업데이트를 막고, 해당 버전을 고정
 sudo apt-mark hold kubelet kubeadm kubectl
